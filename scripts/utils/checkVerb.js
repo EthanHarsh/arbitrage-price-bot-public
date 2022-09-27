d4847a60075372a318f5c8be1a73b008b1ac0a28
Ethan Harsh <ethan@ethanharsh.com>
2022-09-27 12:03:37 -0400

    Update checkVerb.js

--- scripts/utils/checkVerb.js
+++ scripts/utils/checkVerb.js
@@ -1,9 +1,9 @@
-const verb = ((process.env.VERB) && ((process.env.VERB).toLowerCase() === 'true'));
+const verb = process.env.VERB && process.env.VERB.toLowerCase() === "true";
 
 module.exports = checkVerb;
 
 function checkVerb(log) {
-    if(verb) {
-        console.log(log);
-    }
+  if (verb) {
+    console.log(log);
+  }
 }
